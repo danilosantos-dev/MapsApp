554f6c03f885ceb264e609d6049a05c35bb8c775
Danilo Santos <santosdanilo2024@gmail.com>
2023-05-18 20:20:38 -0300

    Adicionado plugin para buscar a localização atual

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { GoogleMap } from '@capacitor/google-maps';
 import { environment } from 'src/environments/environment';
+import { Geolocation, Position } from '@capacitor/geolocation';
+
 
 @Component({
   selector: 'app-home',
@@ -12,9 +14,9 @@ export class HomePage {
   @ViewChild('map') mapRef!: ElementRef<HTMLElement>;
   map!: GoogleMap;
 
-  constructor() {}
+  constructor() { }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.createMap();
   }
 
@@ -25,11 +27,42 @@ export class HomePage {
       apiKey: environment.mapsKey,
       config: {
         center: {
-          lat: 33.6,
-          lng: -117.9,
+          lat: -22.489271,
+          lng: -48.546447,
         },
         zoom: 1,
       },
     });
+    this.buscarPosicao();
   }
-}
+
+  async buscarPosicao() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+    this.adicionarMarcador(coordinates);
+    return coordinates;
+  }
+
+  async adicionarMarcador(coordinates: Position) {
+    // Add a marker to the map
+    const markerId = await this.map.addMarker({
+      coordinate: {
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude
+      }      
+    });
+    this.zoomNoMarcador(coordinates);
+  }
+
+  zoomNoMarcador(coordinates: Position){
+    this.map.setCamera({
+      coordinate:{
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude
+      },
+    zoom:15,
+    animate: true
+    })
+  }
+
+} 
